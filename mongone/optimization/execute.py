8e5775cf14a706700d68479dae7bcfc18b5a30e6
Estrada Irribarra, Rodrigo Andres <rodrigo.estrada@cencosud.cl>
2024-11-16 11:09:07 -0300

    feat: Home with screenshot magnification

--- mongone/optimization/execute.py
+++ mongone/optimization/execute.py
@@ -10,7 +10,6 @@ console = Console()
 BASE_URL = "https://cloud.mongodb.com/api/atlas/v2/groups/{groupId}/clusters/{clusterName}"
 
 # Proxy function to execute plans based on type and environment
-
 def execute_plan(plan_type, environment, plan_filename):
     if not os.path.exists(plan_filename):
         console.print(f"[red]No plan file found for {plan_type} in environment {environment}. Skipping execution.[/]")
@@ -22,6 +21,8 @@ def execute_plan(plan_type, environment, plan_filename):
     # Call the respective function based on plan type
     if plan_type == "autoscaling_computation":
         enable_autoscaling_computation(plan_data)
+    elif plan_type == "autoscaling_disk":
+        enable_autoscaling_disk(plan_data)
     else:
         console.print(f"[red]Unknown plan type: {plan_type}[/]")
 
@@ -72,4 +73,58 @@ def enable_autoscaling_computation(plan_data):
         }
 
         console.print(f"[blue]Enabling auto-scaling computation for cluster {cluster_name} in project {project_id}[/]")
+        make_request(url, data=payload, response_format="json")
+
+# Function to enable auto-scaling disk for clusters
+def enable_autoscaling_disk(plan_data):
+    # Load configuration for autoscaling settings
+    config = load_config()
+    autoscaling_defaults = config.get("autoscaling_defaults", {})
+
+    # Extract default values from configuration or use fallback values
+    provider_name = autoscaling_defaults.get("provider_name", "AWS")
+    region_name = autoscaling_defaults.get("region_name", "US_GOV_WEST_1")
+    max_instance_size = autoscaling_defaults.get("max_instance_size", "M40")
+    min_instance_size = autoscaling_defaults.get("min_instance_size", "M10")
+
+    clusters = plan_data.get("clusters", [])
+    for cluster in clusters:
+        org_id = cluster.get("org_id")
+        project_id = cluster.get("project_id")
+        cluster_name = cluster.get("cluster_name")
+
+        if not org_id or not project_id or not cluster_name:
+            console.print("[red]Missing necessary information to execute plan[/]")
+            continue
+
+        url = BASE_URL.format(groupId=project_id, clusterName=cluster_name)
+
+        # Payload to enable disk auto-scaling
+        payload = {
+            "replicaSetScalingStrategy": "SEQUENTIAL",
+            "replicationSpecs": [
+                {
+                    "regionConfigs": [
+                        {
+                            "providerName": provider_name,
+                            "regionName": region_name,
+                            "autoScaling": {
+                                "compute": {
+                                    "enabled": True,
+                                    "maxInstanceSize": max_instance_size,
+                                    "minInstanceSize": min_instance_size,
+                                    "scaleDownEnabled": True
+                                },
+                                "diskGB": {
+                                    "enabled": True
+                                }
+                            }
+                        }
+                    ],
+                    "zoneName": "string"
+                }
+            ]
+        }
+
+        console.print(f"[blue]Enabling auto-scaling disk for cluster {cluster_name} in project {project_id}[/]")
         make_request(url, data=payload, response_format="json")
